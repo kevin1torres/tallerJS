83925e5651aeaedd4c758f30e1f8377659410a18
kevin1torres <torreskevinsantiago4@gmail.com>
2026-04-09 13:10:34 +0000

    variables

---
+++ simuladordepago.js
@@ -0,0 +1,8 @@
+let nombreCompleto ="";
+let edad = 0 ;
+let tipodeDocumento ="";
+let numeroDocumento ="";
+let salarioEnPesos = 0;
+let comisiones = 0;
+let totalDeHorasExtras = 0;
+let nivelDeRiesgo = "";
